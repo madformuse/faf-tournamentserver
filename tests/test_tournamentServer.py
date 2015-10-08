d9ef84291e2b9caf49f4e95dce92e4116d311ed9
madformuse <tom.howcroft@gmail.com>
2015-10-08 22:42:22 +0100

    Make sure tournament values are mapped correctly

--- tests/test_tournamentServer.py
+++ tests/test_tournamentServer.py
@@ -4,7 +4,6 @@ from tournament.tournamentServer import *
 
 
 class TestTournamentServer:
-
     @pytest.fixture
     def server(self):
         return tournamentServer(db=None)
@@ -12,12 +11,37 @@ class TestTournamentServer:
     def test_create(self, server):
         assert server
 
-    def test_no_tournaments(self,server):
+    def test_no_tournaments(self, server):
         with patch('challonge.tournaments.index', return_value={}) as fake_index:
             server.importTournaments()
 
         assert server.tournaments == {}
 
+    def test_tournament_mapping(self, server):
+        challonge_tournament = {
+            'id': '1',
+            'name': 'test',
+            'full-challonge-url': 'http://www.google.com',
+            'description': 'Amazing',
+            'tournament-type': 'dunno',
+            'progress-meter': '0',
+            'started-at': None,
+            'completed-at': None,
+            'open_signup': None
+        }
 
+        faf_tournament = {
+            'name': challonge_tournament['name'],
+            'url': challonge_tournament['full-challonge-url'],
+            'description': challonge_tournament['description'],
+            'type': challonge_tournament['tournament-type'],
+            'progress': challonge_tournament['progress-meter'],
+            'state': 'open',
+            'participants': []
+        }
 
+        with patch('challonge.tournaments.index', return_value=[challonge_tournament]):
+            with patch('challonge.participants.index', return_value=[]):
+                server.importTournaments()
 
+        assert server.tournaments['1'] == faf_tournament
